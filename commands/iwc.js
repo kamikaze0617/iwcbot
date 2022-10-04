57e354b63c73d7572bc34c5f28b502bf66612c7d
kamikaze0617 <jsmith61799@gmail.com>
2022-10-04 14:44:17 -0400

    Add days remaining until Winter functionality

--- commands/iwc.js
+++ commands/iwc.js
@@ -1,10 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('iwc')
 		.setDescription('Replies with is winter coming info'),
+        
 	async execute(interaction) {
-		await interaction.reply('WIP');
+        const dayjs = require('dayjs');
+        const now = dayjs();
+        const ws = new Date(2022, 11, 22);
+        const winterSolstice = dayjs(ws);
+        var duration = winterSolstice.diff(now, 'days')
+
+        const eow = new Date(2023, 2, 20);
+        const endOfWinter = dayjs(eow);
+        var daysOfWinter = endOfWinter.diff(now, 'days')
+
+        if (duration > 0) {
+		    await interaction.reply('YES. Winter is coming: ' + duration + ' days remain.');
+        } else {
+            await interaction.reply("NO. Winter came and is here for another " + daysOfWinter + ' days.');
+        }
 	},
 };
